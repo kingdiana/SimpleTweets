d1748874261f17f39e4255e39bfe1f821810069b
Diana King <king_diana@yahoo.com>
2015-03-10 12:50:07 -0700

    Initial commit.

---
+++ app/src/main/java/com/codepath/apps/mysimpletweets/TimelineActivity.java
@@ -0,0 +1,61 @@
+package com.codepath.apps.mysimpletweets;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class TimelineActivity extends ActionBarActivity {
+    private TwitterClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timeline);
+        client = TwitterApplication.getRestClient(); // singleton client
+        populateTimeline();
+    }
+
+    private void populateTimeline() {
+        client.getHomeTimeline(new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray json) {
+                Log.d("DEBUG", json.toString());
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                Log.d("DEBUG", errorResponse.toString());
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_timeline, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/codepath/apps/mysimpletweets/models/Tweet.java
@@ -0,0 +1,19 @@
+package com.codepath.apps.mysimpletweets.models;
+
+    /*
+        [{
+            "coordinates": null,
+            "truncated": false,
+            "created_at": "Tue Aug 28 21:16:23 +0000 2012",
+            "favorited": false,
+            "id_str": "240558470661799936",
+            "in_reply_to_user_id_str": null,
+            "entities": {
+              "urls": [],
+              "hashtags": [],
+              "user_mentions": []
+            },
+         */
+public class Tweet {
+
+}
