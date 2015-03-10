2d4609456980329c090d8e3f7c69fc55752914a4
Diana King <king_diana@yahoo.com>
2015-03-10 16:21:07 -0700

    Completed through end of video.

--- app/src/main/java/com/codepath/apps/mysimpletweets/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/mysimpletweets/TimelineActivity.java
@@ -5,20 +5,33 @@ import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
+import com.codepath.apps.mysimpletweets.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.apache.http.Header;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
 public class TimelineActivity extends ActionBarActivity {
     private TwitterClient client;
+    private ArrayList<Tweet> tweets;
+    private TweetsArrayAdapter aTweets;
+    private ListView lvTweets;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
+
+        lvTweets = (ListView) findViewById(R.id.lvTweets);
+        tweets = new ArrayList<>();
+        aTweets = new TweetsArrayAdapter(this, tweets);
+        lvTweets.setAdapter(aTweets);
+
         client = TwitterApplication.getRestClient(); // singleton client
         populateTimeline();
     }
@@ -28,6 +41,7 @@ public class TimelineActivity extends ActionBarActivity {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONArray json) {
                 Log.d("DEBUG", json.toString());
+                aTweets.addAll(Tweet.fromJSONArray(json));
             }
 
             @Override
--- app/src/main/java/com/codepath/apps/mysimpletweets/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/mysimpletweets/models/Tweet.java
@@ -1,19 +1,80 @@
 package com.codepath.apps.mysimpletweets.models;
 
-    /*
-        [{
-            "coordinates": null,
-            "truncated": false,
-            "created_at": "Tue Aug 28 21:16:23 +0000 2012",
-            "favorited": false,
-            "id_str": "240558470661799936",
-            "in_reply_to_user_id_str": null,
-            "entities": {
-              "urls": [],
-              "hashtags": [],
-              "user_mentions": []
-            },
-         */
-public class Tweet {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.util.ArrayList;
+
+/*
+    [{
+        "coordinates": null,
+        "truncated": false,
+        "created_at": "Tue Aug 28 21:16:23 +0000 2012",
+        "favorited": false,
+        "id_str": "240558470661799936",
+        "in_reply_to_user_id_str": null,
+        "entities": {
+          "urls": [],
+          "hashtags": [],
+          "user_mentions": []
+        },
+     */
+public class Tweet {
+    private String body;
+    private  long uid; // unique id for the tweet
+    private User user;
+    private String createdAt;
+
+    public String getBody() {
+        return body;
+    }
+
+    public long getUid() {
+        return uid;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    // Deserialize a tweet from JSON.
+    public static Tweet fromJSON(JSONObject jsonObject) {
+        Tweet tweet = new Tweet();
+
+        try {
+            tweet.body = jsonObject.getString("text");
+            tweet.uid = jsonObject.getLong("id");
+            tweet.createdAt = jsonObject.getString("created_at");
+            tweet.user = User.fromJSON(jsonObject.getJSONObject("user"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return tweet;
+    }
+
+    public static ArrayList<Tweet> fromJSONArray(JSONArray jsonArray) {
+        ArrayList<Tweet> tweets = new ArrayList<>();
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            try {
+                JSONObject tweetJson = jsonArray.getJSONObject(i);
+                Tweet tweet = Tweet.fromJSON(tweetJson);
+
+                if (tweet != null) {
+                    tweets.add(tweet);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                continue;
+            }
+        }
+
+        return tweets;
+    }
 }
